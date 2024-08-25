c3e1e8825a08da49f254903eddc2a7045915c142
javier.sanchez <kornjaviersanchezb@gmail.com>
2024-08-25 17:21:04 -0500

    actualizado Readme

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/personaje',
+          destination: 'https://ezoiv3yqz6h5sh37ytuva7ns5i0moztz.lambda-url.us-east-2.on.aws', // Ajusta al puerto correcto si es necesario
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
+  
